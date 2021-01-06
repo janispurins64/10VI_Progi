aaa34ce9db581be16f6d432514520bac0ff8691d
janispurins64 <janispurin64@gmail.com>
2021-01-06 10:54:51 +0200

    Kontroļi
    Viens tekstlauciņš

--- galva.py
+++ galva.py
@@ -14,9 +14,19 @@ def root():
     bb = float(request.args.get('b',default='0.',type=str))
     cc = float(request.args.get('c',default='0.',type=str))
     
-    rez = "Perimetrs=" + funcy.tr_per(aa,bb,cc)
+    rez = "Perimetrs=" + funci.tr_per(aa,bb,cc)
     return render_template("sveikaPasaule.html",vards="Trīsstūra parametri",rezultats=rez)
 
+@app.route('/pogasall',methods=['GET'])
+def pogasall():
+    aa = request.args.get('a',default='0.',type=str)
+    aa= "Ievadītā vērtība: " + aa
+    
+    return render_template("sveikaPasaule.html",vards="Kontroļu pārbaude",rezultats=aa)
+
+@app.route('/pogas')
+def pogas():
+    return render_template("pogas.html",vards="Podziņas")
 
 @app.route('/tests')
 def health():
